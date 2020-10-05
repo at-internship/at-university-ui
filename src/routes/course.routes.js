2967be5840754cf8ee7c49759b455c6cf61adc8f
majo201429 <maria.yanez@agilethought.com>
2020-10-05 10:25:40 -0500

    modifications in controllers and routes

--- src/routes/course.routes.js
+++ src/routes/course.routes.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
-const { getAllCourses } = require ('../controllers/TestController'); 
+const { getAllCourses } = require ('../controllers/test.controller'); 
 const { renderCourses } = require ('../controllers/dashboard.controller');
 
 /* Routes */
 
 //Get All Courses 
-router.get("/course",getAllCourses);
+router.get("/course", getAllCourses);
 
 //Get Dashboard 
 router.get("/dashboard", renderCourses);
