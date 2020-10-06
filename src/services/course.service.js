fc5269410fb2d599e0e5afb48f33829464e63a0a
HumbertoGonzalez-AT <71738130+hgglez@users.noreply.github.com>
2020-10-05 22:41:27 -0500

    se creo un nuevo metodo al curso con axios

--- src/services/course.service.js
+++ src/services/course.service.js
@@ -1,1 +1,20 @@
+const axios = require("axios");
+const config = require("../config");
 
+const courseService = {};
+
+courseService.getAllCourses = () => {
+  return axios({
+    method: "GET",
+    url: config.AT_UNIVERSITY_API_HOST + `/course`,
+  });
+};
+
+courseService.addAllCourses = () => {
+  return axios({
+    method: "POST",
+    url: config.AT_UNIVERSITY_API_HOST + `/course`,
+  });
+};
+
+module.exports = courseService;
