c382362795428378c05a35b207c5ae32907bef1b
Erick Barreto <erick.barreto@agilethought.com>
2020-10-15 10:40:02 -0500

    it was renamed the functions existing and add new functions without functionality in the admin controller

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,15 +1,16 @@
 const { addCourse } = require("../services/course.service")
+const { getAllCourses } = require("../services/course.service");
 const adminCtrl = {};
 
-adminCtrl.renderAdminDash = (req, res) => {
+adminCtrl.renderIndexAdmin = (req, res) => {
     res.render('admin/dashboard');
 }
 
-adminCtrl.renderCourseForm = (req, res) => {
+adminCtrl.renderAddCourseForm = (req, res) => {
     res.render('admin/new-course');
 }
 
-adminCtrl.createNewCourse = async (req, res) => {
+adminCtrl.addCourse = async (req, res) => {
     const { title, description, status, category, img } = req.body;
     let courses;
     let request = {
@@ -23,8 +24,29 @@ adminCtrl.createNewCourse = async (req, res) => {
         console.log(result);
         courses = result;
     });
-
+    
     res.redirect("/dashboard");
 };
+
+adminCtrl.renderCourseList = async (req, res) => {
+    let courses;
+	await getAllCourses().then((result) => {
+		courses = result.data;
+	});
+	res.render("dashboard/all-courses/", { courses });
+};
+
+adminCtrl.renderEditCourseForm = (req, res) => {
+    res.render('admin/edit-course');
+}
+
+adminCtrl.updateCourse = (req, res) => {
+    res.render('admin/update-course');
+}
+
+adminCtrl.deleteCourse = (req, res) => {
+    res.render('dashboard/all-courses');
+}
+
 
 module.exports = adminCtrl;
