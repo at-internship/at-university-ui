f137ae100ff575d00a0f41eb977f26c6baff6db0
Yair Martinez <yair.martinez@agilethought.com>
2020-10-15 12:01:44 -0500

    Update admin.routes.js

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -10,5 +10,42 @@ router.get('/course', renderCourseForm)
 
 router.post('/course', createNewCourse);
 
+// Admin Controller
+const {
+    renderCourseList,
+    renderAddCourseForm,
+    addCourse,
+    renderEditCourseForm,
+    updateCourse,
+    deleteCourse
+} = require("../controllers/admin.controller");
+
+// ============= Sub Routes =============
+
+// AT-UNIVERSITY-UI - Admin - Index
+router.get("/admin", renderIndexAdmin);
+
+
+// AT-UNIVERSITY-UI - Admin - Users - Render Course List
+router.get("/admin/course", renderCourseList);
+
+
+// AT-UNIVERSITY-UI - Admin - Users - Render Add Course
+router.get("/admin/course/add", renderAddCourseForm);
+
+
+// AT-UNIVERSITY-UI - Admin - Users - Add Course
+router.post("/admin/course/add", addCourse);
+
+
+// AT-UNIVERSITY-UI - Admin - Users - Render Edit Curse
+router.get("/admin/course/edit/:id", renderEditCourseForm);
+
+// AT-UNIVERSITY-UI - Admin - Users - Edit User
+router.put("/admin/course/edit/:id", updateCourse);
+
+// AT-UNIVERSITY-UI - Admin - Courses - Delete User
+router.get("/admin/course/delete/:id", deleteCourse);
+
 
 module.exports = router;
