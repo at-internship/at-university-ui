d26eac7160a22d4eb96eb53b092619c701b7a651
Oscar Sanchez <oscar.sanchez@agilethought.com>
2020-10-27 10:03:25 -0600

    AT-UNIVERSITY-UI Course Details

--- src/controllers/at-university.controller.js
+++ src/controllers/at-university.controller.js
@@ -4,8 +4,34 @@ const universityServiceAPI = require("../services/at-university-api.service");
 
 // AT-RESOURCES - Index/Dashboard
 atUniversityCtrl.dashboard = async(req, res) => {
-    console.log("--> atUniversityCtrl.dashboard");
-    res.render("dashboard");
+    try {
+        const responseCourses = await universityServiceAPI.getAllCourses();
+        console.log("--> atUniversityCtrl.dashboard.getAllCourses");
+        console.log(responseCourses.data);
+        const courses = responseCourses.data
+        res.render("dashboard", { courses });
+    } catch (err) {
+        console.error(err.message)
+        res.render("dashboard");
+    }
+};
+
+// AT-RESOURCES - Career
+atUniversityCtrl.careerDetails = async(req, res) => {
+    console.log("--> atUniversityCtrl.career");
+    res.render("career/details");
+};
+
+// AT-RESOURCES - Course
+atUniversityCtrl.courseDetails = async(req, res) => {
+    console.log("--> atUniversityCtrl.course");
+    res.render("course/details");
+};
+
+// Start Course
+atUniversityCtrl.startCourse = (req, res) => {
+    console.log("--> atUniversityCtrl.startCourse");
+    res.render("course/start");
 };
 
 module.exports = atUniversityCtrl;
