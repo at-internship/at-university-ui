af0345e4f5d8dc23b5facf828a921fd3c98786ec
HumbertoGonzalez-AT <71738130+hgglez@users.noreply.github.com>
2020-11-17 18:59:00 -0600

    Validation PUT

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -10,16 +10,16 @@ adminCtrl.renderIndexAdmin = (req, res) => {
 
 // AT-UNIVERSITY - Admin - Render Course List
 adminCtrl.renderCourseList = async (req, res) => {
-    try {
-        const responseCourses = await universityServiceAPI.getAllCourses();
-        console.log("---> adminCtrl.renderCourseList.getAllCourses");
-        //console.log(responseCourses.data);
-        const courses = responseCourses.data;
-        res.render("admin/course/index", { courses });
-    } catch (err) {
-        console.error(err.message);
-        res.render("admin/course/index");
-    }
+  try {
+    const responseCourses = await universityServiceAPI.getAllCourses();
+    console.log("---> adminCtrl.renderCourseList.getAllCourses");
+    //console.log(responseCourses.data);
+    const courses = responseCourses.data;
+    res.render("admin/course/index", { courses });
+  } catch (err) {
+    console.error(err.message);
+    res.render("admin/course/index");
+  }
 };
 
 // AT-UNIVERSITY - Admin - Render Add Course Form
@@ -29,59 +29,61 @@ adminCtrl.renderAddCourseForm = (req, res) => {
 
 // AT-UNIVERSITY - Admin - Add Course
 adminCtrl.addCourse = async (req, res) => {
-    const { title, description, status, category, img } = req.body;
-    let courses;
-    let request = {
-        title,
-        description,
-        category: category,
-        img,
-        status: parseInt(status),
-    };
-    await universityServiceAPI.addCourse(request).then(result => {
-        console.log(result);
-        courses = result;
-    });
+  const { title, description, status, category, img } = req.body;
+  let courses;
+  let request = {
+    title,
+    description,
+    category: category,
+    img,
+    status: parseInt(status),
+  };
+  await universityServiceAPI.addCourse(request).then((result) => {
+    console.log(result);
+    courses = result;
+  });
 
   res.redirect("/admin/course");
 };
 
 // AT-UNIVERSITY - Admin - Render Edit Course Form
 adminCtrl.renderEditCourseForm = async (req, res) => {
-    console.log("--> adminCtrl.renderEditCourseForm");
-    let courseId = req.params.id;
-    universityServiceAPI.getCourseById(courseId);
+  console.log("--> adminCtrl.renderEditCourseForm");
+  let courseId = req.params.id;
+  universityServiceAPI.getCourseById(courseId);
 
-    // Temporary code to retrive information about the course
+  // Temporary code to retrive information about the course
 
-    let responseCourses = await universityServiceAPI.getAllCourses();
+  let responseCourses = await universityServiceAPI.getAllCourses();
 
-    courseDetails = responseCourses.data.filter(function (c) { return c._id == courseId; });
+  courseDetails = responseCourses.data.filter(function (c) {
+    return c._id == courseId;
+  });
 
-    console.log("One course found", courseDetails[0]);
+  console.log("One course found", courseDetails[0]);
 
-    // ---
+  // ---
 
-    res.render("admin/course/edit-course", courseDetails[0]);
+  res.render("admin/course/edit-course", courseDetails[0]);
 };
 
 // AT-UNIVERSITY - Admin - Update Course
 adminCtrl.updateCourse = async (req, res) => {
-    // Redirect
-    const { title, description, status, category, img } = req.body;
-    const _id = req.params.id;
+  // Redirect
+  const { title, description, status, category, img } = req.body;
+  const _id = req.params.id;
 
-    await universityServiceAPI.updateCourse({
-        _id: _id,
-        title: title,
-        description: description,
-        category: category,
-        img: img,
-        status: parseInt(status)
-    })
+  await universityServiceAPI.updateCourse({
+    _id: _id,
+    title: title,
+    description: description,
+    category: category,
+    img: img,
+    status: parseInt(status),
+  });
 
-    req.flash("success_msg", "Course Updated Successfully");
-    res.redirect("/admin/course");
+  req.flash("success_msg", "Course Updated Successfully");
+  res.redirect("/admin/course");
 };
 
 // AT-UNIVERSITY - Admin - Delete Course
@@ -95,7 +97,32 @@ adminCtrl.deleteCourse = (req, res) => {
   // Redirect
   req.flash("success_msg", "Course Deleted Successfully");
   res.redirect("/admin/course");
+};
 
+adminCtrl.updateCourse = async (req, res) => {
+  // Redirect
+  try {
+    const { title, description, status, category, img } = req.body;
+    const _id = req.params.id;
+
+    await universityServiceAPI.updateCourse({
+      _id: _id,
+      title: title,
+      description: description,
+      category: null,
+      img: img,
+      status: parseInt(status),
+    });
+
+    req.flash("success_msg", "Course Updated Successfully");
+  } catch (err) {
+    console.log(err.response);
+    if (err.response && err.response.data) {
+      let errorMsg = err.response.data.message;
+      req.flash("error_msg", errorMsg);
+    }
+  }
+  res.redirect("/admin/course");
 };
 
 module.exports = adminCtrl;
