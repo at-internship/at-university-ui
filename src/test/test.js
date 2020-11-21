1b59ac4b2acae2f2cb32f0e1f18dea1a90357867
HumbertoGonzalez-AT <71738130+hgglez@users.noreply.github.com>
2020-11-21 13:30:48 -0600

    test poc code coverage

---
+++ src/test/test.js
@@ -0,0 +1,20 @@
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+const expect = require("chai").expect;
+
+chai.use(chaiHttp);
+
+const url = "https://at-university-api.herokuapp.com/api" + "/v1/course";
+
+describe("Get all courses: ", () => {
+  it("Should get all courses", (done) => {
+    chai
+      .request(url)
+      .get("/")
+      .end(function (err, res) {
+        console.log(res.body);
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+});
